aa8433ce2dd90ac5b89afbf179073a7af12b093d
Ruma303 <matteoh8@hotmail.it>
2024-02-03 22:56:24 +0100

    update

--- struct.rs
+++ struct.rs
@@ -1,3 +1,21 @@
-fn main {
-
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
 }
+
+    fn main() {
+        let mut user1 : User = User {
+            email: String::from("user@1.com"),
+            username: String::from("user1"),
+            active: true,
+            sign_in_count: 1,
+        };
+        user1.active = false; //* ok
+
+        let user_email = user1.email;
+        println!("{user_email}"); // user@1.com
+
+    }
+
